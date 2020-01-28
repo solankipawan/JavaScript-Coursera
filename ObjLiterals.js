f06f996b90de4623c5a14789e936ec9c51e8d52d
P_Solanki29 <pksaug2529@gmail.com>
2020-01-28 22:43:19 +0530

    Object Literals and the 'this' Keyword

---
+++ ObjLiterals.js
@@ -0,0 +1,18 @@
+var literalCircle = {
+    radius: 10,
+
+    getArea: function() {
+        var self = this;
+        console.log(this);
+
+        var increaseRadius = function() {
+            self.radius = 20;
+        };
+        increaseRadius();
+        console.log(this.radius);
+
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+};
+
+console.log(literalCircle.getArea());
